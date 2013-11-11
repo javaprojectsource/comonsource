165e412d59420fadac132e2e054af6c19b3240c0
admin <admin@admin-PC>
2013-11-11 12:00:17 +0900

    3st

--- commonSource/src/kr/co/javaproject/commonSource/StringUtils.java
+++ commonSource/src/kr/co/javaproject/commonSource/StringUtils.java
@@ -1,10 +1,10 @@
 package kr.co.javaproject.commonSource;
 /**
  * StringUtil 
- * String 처리에 대한 Util
+ * <p>String 처리에 대한 Util</p>
  * @author www.javaproject.co.kr 
  * 소스의 자유로운 사용,변경,재 배포가 가능합니다.
- * 자바프로젝트사이트는 본 소스 사용함에 있어 모든 결과물에 대한 책임은 지지 않습니다.
+ * 자바프로젝트사이트는 본 소스를 사용함에 있어 모든 결과물에 대한 책임은 지지 않습니다.
  * 소스에 대한 버그 사항은 사이트 게시판을 통해 남겨주세요.   
  */
 public class StringUtils {
@@ -12,7 +12,7 @@ public class StringUtils {
 	
 	/**
 	 *  Null 체트를 한다
-	 *  사용법 : if(StringUtils.isNull(str)) { }
+	 * <p>  사용법 : if(StringUtils.isNull(str)) { }</p>
 	 * @param val 널 체크를 할  값
 	 * @return
 	 */
@@ -22,7 +22,7 @@ public class StringUtils {
 	
 	/**
 	 * String 값으로 변환한다.
-	 * 사용법 : String s = StringUtils.stringValue(str);
+	 * <p>사용법 : String s = StringUtils.stringValue(str);</p>
 	 * @param val String value로 변환 할 값
 	 * @return
 	 */
@@ -32,7 +32,7 @@ public class StringUtils {
 	
 	/**
 	 * String 값으로 변환하고  null일 경우 String 값을 세팅 한다
-	 * 사용법 : String s = StringUtils.stringValue(str,"abc");
+	 * <p>사용법 : String s = StringUtils.stringValue(str,"abc");</p>
 	 * @param val
 	 * @param val2
 	 * @return
@@ -43,7 +43,7 @@ public class StringUtils {
 	
 	/**
 	 * String 겂을 int 값으로 변환한다
-	 * 사용법 : int a = StringUtils.intValue(str);
+	 *  <p>사용법 : int a = StringUtils.intValue(str);</p>
 	 * @param val
 	 * @return
 	 */
@@ -54,7 +54,7 @@ public class StringUtils {
 	
 	/**
 	 * String 겂을 int 값으로 변환한다
-	 * 사용법 : int a = StringUtils.intValue(str,5);
+	 * <p>사용법 : int a = StringUtils.intValue(str,5);</p>
 	 * @param val
 	 * @return
 	 */
