93fa2404de35e365dcc0eed1d79ffdcdced545b7
admin <admin@admin-PC>
2013-11-11 13:26:08 +0900

    Test 추가

---
+++ commonSource/src/kr/co/javaproject/commonSource/Test.java
@@ -0,0 +1,13 @@
+package kr.co.javaproject.commonSource;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+		int a = StringUtils.intValue("11a",6);
+		System.out.println("a = " + a);
+		
+	}
+
+}
